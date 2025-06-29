4669bc4b445c7696a01a5cc402a4673f6439c70d
Yeiner Josué Villegas Li <yeiner.villegasli@airbrij.com>
2025-06-28 20:00:38 -0600

    feat: cardComponent and fix: styles for input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import './App.css'
 import InputComponent from './components/inputComponent'
 import ButtonComponent from './components/buttonComponent';
+import CardComponent from './components/cardComponent';
 import { useState } from 'react';
 
 function App() {
   const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
       <div>
-        <InputComponent
-          value={userName}
-          placeholder={"Username"}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <div className='mt-[16px]'>
-        <ButtonComponent title="Login" onClick={() => {}}
-          className='bg-pink-500'
-          />
-        </div>
+        <CardComponent>
+          <div className='flex flex-col gap-4'>
+            <InputComponent
+              value={userName}
+              placeholder={"Username"}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <InputComponent
+              value={password}
+              placeholder={"Password"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className='mt-[16px]'>
+            <ButtonComponent title="Login" onClick={() => { }} />
+          </div>
+        </CardComponent>
       </div>
     </>
   )
