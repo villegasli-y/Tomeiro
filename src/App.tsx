9819dd876b66233d50410f868b534bd7ad5470ba
Yeiner Josué Villegas Li <yeiner.villegasli@airbrij.com>
2025-07-03 16:50:31 -0600

    feat: logout in 5 minutes to test useEffect hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import InputComponent from './components/inputComponent'
 import ButtonComponent from './components/buttonComponent';
 import CardComponent from './components/cardComponent';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [username, setUserName] = useState("");
@@ -18,16 +18,37 @@ function App() {
     }
   }
 
+  const handleLogOut = () => {
+    setUser(null);
+    setUserName("");
+    setPassword("");
+    setIsLoggedIn(false);
+  }
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    if (isLoggedIn) {
+      timeoutId = setTimeout(() => {
+        handleLogOut();
+      }, 5 * 60 * 1000);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [isLoggedIn])
+
   return (
     <div>
       {isLoggedIn ? (
         <div className='animate-fade-in text-center text-xl font-bold text-black'>
           <pre>{JSON.stringify(user)}</pre>
-          <p>Welcome {user?.username}!!!</p> 
+          <p>Welcome {user?.username}!!!</p>
           {/* TODO: crear WelcomePage y sustituirlo aquí */}
         </div>
       ) : (
-        <form onSubmit={handleLoginSubmit}>
+        <form onSubmit={handleLoginSubmit} className='animate-fade-in'>
           <CardComponent>
             <div className='flex flex-col gap-4'>
               <InputComponent
