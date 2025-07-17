9a254992a51867b3f4246e193f5d6bb366c84b96
Yeiner Josué Villegas Li <yeiner.villegasli@airbrij.com>
2025-07-16 18:04:02 -0600

    feat: quick test of useRef to use focus in login page in the form input

--- src/components/inputComponent.tsx
+++ src/components/inputComponent.tsx
@@ -1,23 +1,26 @@
+import React, { forwardRef } from 'react';
+
 type Props = {
     placeholder: string;
     value: string;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export default function InputComponent({
-    placeholder,
-    value,
-    onChange
-}: Props) {
-    return (
-        <div>
-            <input
-                type="text"
-                className="w-full px-4 py-2 border bg-white text-black border-gray-400 rounded-md focus:outline-none
+const InputComponent = forwardRef<HTMLInputElement, Props>(
+    ({ placeholder, value, onChange }, ref) => {
+        return (
+            <div>
+                <input
+                    ref={ref}
+                    type="text"
+                    className="w-full px-4 py-2 border bg-white text-black border-gray-400 rounded-md focus:outline-none
                 focus:ring-2 focus:ring-blue-500 focus:border-transparent placeholder:text-center placeholder:text-slate-500"
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange} />
-        </div>
-    )
-}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange} />
+            </div>
+        )
+    }
+);
+
+export default InputComponent;
