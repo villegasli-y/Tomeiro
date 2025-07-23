94b67f6ac284aed6ab042cdf0266dd65e6538248
Yeiner Josué Villegas Li <yeiner.villegasli@airbrij.com>
2025-07-22 19:07:46 -0600

    feat: AppBar Component

--- src/components/cardComponent.tsx
+++ src/components/cardComponent.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function CardComponent({ children }: Props) {
 
     return (
-        <div className="bg-white border-2 w-[396px] h-full rounded-lg p-4 " style={{ boxShadow: '0 2px 4px #0000001a, 0 8px 16px #0000001a' }}>
+        <div className="bg-white border-2 w-[396px] rounded-lg p-4 " style={{ boxShadow: '0 2px 4px #0000001a, 0 8px 16px #0000001a' }}>
             {children}
         </div>
     )
