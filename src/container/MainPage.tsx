ae129d7f0ab54ec5602d22e145c286ead0b58f61
Yeiner Josué Villegas Li <yeiner.villegasli@airbrij.com>
2025-08-05 20:38:58 -0600

    feat: UI for timer

--- src/container/MainPage.tsx
+++ src/container/MainPage.tsx
@@ -1,11 +1,18 @@
+import TimerComponent from "@/components/TimerComponent"
+
 function MainPage() {
     return (
-         <div className='flex flex-col justify-center text-center items-center gap-4'>
-            <div className='font-bold'>
-                <h1>Welcome to Tomeiro Proyect</h1>
+        <>
+            <div className='w-full flex flex-col justify-center text-center items-center gap-4'>
+                <div className='font-bold'>
+                    <h1>Welcome to Tomeiro Proyect</h1>
+                </div>
+                <img src="public/tomeiro_favicon.png" alt="tomeiro logo" width={200} height={200} />
+                <div className="">
+                    <TimerComponent />
+                </div>
             </div>
-            <img src="public/tomeiro_favicon.png" alt="tomeiro logo" width={200} height={200} />
-        </div>
+        </>
     )
 }
 
